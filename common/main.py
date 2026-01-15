6060c5dc9d42d263b7ae93cda062da6a579d9263
pedroleo <pedroleo@ucm.es>
2026-01-15 23:32:13 +0100

    Mostrar número de eventos

--- common/main.py
+++ common/main.py
@@ -7,10 +7,17 @@ def main():
 	folktales_json = load_json_folder(out_dir)
 	folktales = [AnnotatedFolktale(**folktale_json) for folktale_json in folktales_json.values()]
 	
+	valid_foktales: list[AnnotatedFolktale] = []
 	for filename, folktale in zip(folktales_json.keys(), folktales):
 		n_events = len(folktale.events)
 		if n_events <= MIN_EVENTS:
 			print(f"'{filename}' has too few events ({n_events}, minimum required: {MIN_EVENTS})")
+		else:
+			valid_foktales.append(folktale)
+
+	average_eventes = sum(len(folktale.events) for folktale in valid_foktales) / len(valid_foktales)
+	print(f"Average number of events per folktale: {average_eventes}.")
+
 	show_genre_distribution(folktales)
 
 if __name__ == "__main__":
